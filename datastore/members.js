e4960693a2a9d40b4d2579c5bedb979206601127
Simon Gomes <busy.s.simon@gmail.com>
2022-03-04 17:43:18 +0600

    initial commit

---
+++ datastore/members.js
@@ -0,0 +1,62 @@
+export const members = [
+  {
+    name: "ম্যানুয়াল ভিকি গমেজ",
+    image: "/assets/members/vicky.jpg",
+    position: "সভাপতি",
+  },
+  {
+    name: "রাফায়েল বিশাল গমেজ",
+    image: "/assets/members/Bishal.JPG",
+    position: "সহ-সভাপতি",
+  },
+  {
+    name: "আন্তুনী আকাশ গমেজ",
+    image: "/assets/members/akash.jpg",
+    position: "সাধারণ সম্পাদক",
+  },
+  {
+    name: "আন্তুনী স্বপনীল গমেজ",
+    image: "/assets/members/swapnil.jpg",
+    position: "সহ-সাধারণ সম্পাদক",
+  },
+  {
+    name: "এলবার্ট তন্ময় গমেজ",
+    image: "/assets/members/tonmoy.jpg",
+    position: "কোষাধ্যক্ষ",
+  },
+  {
+    name: "জেরাল্ড মিঠু গমেজ",
+    image: "/assets/members/mithu.jpg",
+    position: "ক্রীয়া সম্পাদক",
+  },
+  {
+    name: "হেনরী টকি গমেজ",
+    image: "/assets/members/toky.jpg",
+    position: "সহ-ক্রীয়া সম্পাদক",
+  },
+  {
+    name: "সাইমন সুমিত গমেজ",
+    image: "/assets/members/sumit.jpg",
+    position: "তথ্য ও প্রযুক্তি সম্পাদক",
+  },
+  {
+    name: "বারবারা রিক্তা গমেজ",
+    image: "/assets/members/rikta.jpg",
+    position: "শিক্ষা ও সাংস্কৃতিক সম্পাদক",
+  },
+  {
+    name: "রোজমেরী প্রিয়াঙ্কা গমেজ",
+    image: "/assets/members/prianka.jpg",
+    position: "সহ-শিক্ষা ও সাংস্কৃতিক সম্পাদক",
+  },
+  {
+    name: "মার্ক পিয়াল গমেজ",
+    image: "/assets/members/pial.jpg",
+    position: "ধর্মীয় সম্পাদক",
+  },
+  {
+    name: "সেবাস্টিয়ান রাহুল গমেজ",
+    image: "/assets/members/rahul.jpeg",
+    position: "যোগাযোগ সম্পাদক",
+  },
+];
